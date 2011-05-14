593296abc57a34c40138d654271f83deb9b5a2a7
Wojciech Meyer <wojciech.meyer@gmail.com>
2011-05-15 00:36:36 +0100

    Inital commit.

---
+++ react_dom.ml
@@ -0,0 +1,35 @@
+open Lwt_react
+open Dom_html
+
+let ticks ms =
+  let signal, send_time = S.create 0. in
+  let time = ref 0. in
+  let rec timeout_id = ref (lazy (window ## setTimeout (Js.wrap_callback loop, ms)))
+  and loop ev =
+    send_time !time;
+    time := !time +. ms;
+    timeout_id := (lazy window ## setTimeout (Js.wrap_callback loop, ms));
+    ignore (Lazy.force !timeout_id);
+  in
+  ignore (Lazy.force !timeout_id);
+  signal, (fun () -> window ## clearTimeout (Lazy.force !timeout_id))
+
+
+let input_value value (element : inputElement Js.t) =
+  let event, send_event = S.create value in
+  element ## value <- Js.string value;
+  element ## onchange <-
+    (handler (fun ev -> send_event (Js.to_string (element ## value)); Js._false));
+  event
+
+let mouse () =
+  let event, send_event = S.create (0,0) in
+  let receive ev =
+    let mx, my = eventAbsolutePosition ev in
+    send_event (mx, my);
+    Js._false
+  in
+  addEventListener (document :> eventTarget Js.t) Event.mousemove (handler receive) Js._false;
+  event
+  
+
