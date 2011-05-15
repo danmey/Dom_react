d99daa397e3f2d9f1c561ceb8587b238a54ef858
Wojciech Meyer <wojciech.meyer@gmail.com>
2011-05-15 23:29:55 +0100

    More progress on animation.

--- examples/follow-the-mouse/follow.ml
+++ examples/follow-the-mouse/follow.ml
@@ -12,7 +12,7 @@ let iter f lst =
 let onload ev =
   let (>>=) = Js.Opt.bind in
 
-  (* let delay = 300. in *)
+  let delay = 300. in
 
   let body = Html.document##getElementById (js"body") in
   
@@ -40,7 +40,7 @@ let onload ev =
   in
 
   let mouse = Rd.mouse () in
-  ignore(body >>= 
+  ignore(Html.document##getElementById (js"body") >>= 
            (fun body ->
              ignore (Rd.appendChild body
                (R.S.l1 (fun (x, y) ->
@@ -54,10 +54,11 @@ let onload ev =
                    ~padding:"10px" [ (* Html.createTextinput ~name:(js "the mouse!") Html.document *)] 
                  in
                 (gen(), (d :> Dom.node Js.t)))  mouse));
-             Html.document##getElementById (js"themouse") >>= fun themouse ->
+
+             Html.document ## getElementById (js"themouse") >>= fun themouse ->
              let mouse_offset = themouse ## offsetWidth in
              let tail_pos = 
-               Rd.delay (R.S.l1 (fun (x, y) -> (x + mouse_offset), y) mouse) 200. in
+               Rd.delay (R.S.l1 (fun (x, y) -> (x + mouse_offset), y) mouse) delay in
              Rd.appendChild body
                (R.S.l1 (fun (x, y) ->
                  let d = div
@@ -70,58 +71,34 @@ let onload ev =
                    ~padding:"10px" [ (* Html.createTextarea ~name:(js "the mouse!") Html.document *)] 
                  in
                 (gen(), (d :> Dom.node Js.t))) tail_pos);
-               Js.Opt.return ()));
+
+           let wag_delay = delay *. 1.5 in
+           Html.document ## getElementById (js"tail") >>= fun tail ->
+           let mouseandtail_offset = mouse_offset + tail ## offsetWidth in
+           (* let wag_offset = F.hold 0 (F.collect (fun _ _ -> (Random.int 10) - 5) 0 (Fd.ticks 100.)) in *)
+           let ticks, _ = Rd.ticks 100. in
+           let wag_offset = R.S.map (fun _ -> Random.int 10 - 5) ticks
+           in
+           let wag_pos =
+             R.S.l2
+               (fun (x, y) wag_offset ->
+                 (x + mouseandtail_offset, y + wag_offset))
+               (Rd.delay mouse wag_delay) wag_offset in
+             Rd.appendChild body
+               (R.S.l1 (fun (x, y) ->
+                 let d = div
+                   ~id:"tail"
+                   ~color:"#FFFF00"
+                   ~backgroundColor:"#000000"
+                   ~position:"absolute"
+                   ~left:(string_of_int x)
+                   ~top:(string_of_int y)
+                   ~padding:"10px" [ (* Html.createTextarea ~name:(js "the mouse!") Html.document *)] 
+                 in
+                (gen(), (d :> Dom.node Js.t))) wag_pos);
+           Js.Opt.return ()));
+
   Js._false
-
-
-(*   Fd.appendChild body *)
-(*     (F.blift tail_pos (fun (x, y) -> *)
-(*       div *)
-(*         ~id:"tail" *)
-(*         ~color:"#FF0000" *)
-(*         ~backgroundColor:"#000000" *)
-(*         ~position:"absolute" *)
-(*         ~left:(string_of_int x) *)
-(*         ~top:(string_of_int y) *)
-(*         ~padding:"10px" *)
-(*         [ D.document#createTextNode "its tail!" ])); *)
-
-
-(*   let mouse_offset = (D.document#getElementById "themouse")#_get_offsetWidth in *)
-(*   let tail_pos = F.blift (Fd.delay_b mouse delay) (fun (x, y) -> (x + mouse_offset, y)) in *)
-
-(*   Fd.appendChild body *)
-(*     (F.blift tail_pos (fun (x, y) -> *)
-(*       div *)
-(*         ~id:"tail" *)
-(*         ~color:"#FF0000" *)
-(*         ~backgroundColor:"#000000" *)
-(*         ~position:"absolute" *)
-(*         ~left:(string_of_int x) *)
-(*         ~top:(string_of_int y) *)
-(*         ~padding:"10px" *)
-(*         [ D.document#createTextNode "its tail!" ])); *)
-
-(*   let wag_delay = delay *. 1.5 in *)
-(*   let mouseandtail_offset = mouse_offset + (D.document#getElementById "tail")#_get_offsetWidth in *)
-(*   let wag_offset = F.hold 0 (F.collect (fun _ _ -> (Random.int 10) - 5) 0 (Fd.ticks 100.)) in *)
-(*   let wag_pos = *)
-(*     F.blift2 *)
-(*       (Fd.delay_b mouse wag_delay) wag_offset *)
-(*       (fun (x, y) wag_offset -> *)
-(*         (x + mouseandtail_offset, y + wag_offset)) in *)
-
-(*   Fd.appendChild body *)
-(*     (F.blift wag_pos (fun (x, y) -> *)
-(*       div *)
-(*         ~id:"wagging" *)
-(*         ~color:"#FFFF00" *)
-(*         ~backgroundColor:"#000000" *)
-(*         ~position:"absolute" *)
-(*         ~left:(string_of_int x) *)
-(*         ~top:(string_of_int y) *)
-(*         ~padding:"10px" *)
-(*         [ D.document#createTextNode "is happy!" ])); *)
 
 (* ;; *)
 ;;
