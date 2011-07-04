69f21a2ef6608c875e5b14878c18d3105615efb0
Wojciech Meyer <wojciech.meyer@example.com>
2011-07-04 02:18:29 +0100

    New better interface. mli file.

---
+++ examples/react_event/react_event.ml
@@ -0,0 +1,24 @@
+let js = Js.string
+
+let onload ev =
+  let (>>=) = Js.Opt.bind in
+  let select, attacher = Dom_react.Dom_html2.createSelect Dom_html.document in
+  let ev = attacher (`Onchange, Dom_react.EventType.int) in
+  (Dom_html.document##getElementById (js"body") >>=
+     (fun body ->
+       Dom.appendChild body (select :> Dom.node Js.t);
+       let opt1 = Dom_html.createOption Dom_html.document in
+       let opt2 = Dom_html.createOption Dom_html.document in
+       opt1 ## value <- js"ala ma kota";
+       opt2 ## value <- js"a kot ma ale";
+       select ##  add (opt1, Js.Opt.empty);
+       select ##  add (opt2, Js.Opt.empty);
+       Dom.appendChild select (opt1 :> Dom.node Js.t);
+       Dom.appendChild select (opt1 :> Dom.node Js.t);
+       React.E.map (fun i -> Dom_html.window ## alert (js (Printf.sprintf "change: %d" i))) ev;
+       Js.Opt.return ()));
+  Js._false
+;;
+Dom_html.window##onload <- (Dom_html.handler onload)
+  
+  
