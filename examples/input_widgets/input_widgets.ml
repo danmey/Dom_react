6690087b4313c464267e4cde781ec76dea4e2417
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-01-01 22:47:15 +0000

    Add input_widgets example as a playfield for the new Dom_react Input module

---
+++ examples/input_widgets/input_widgets.ml
@@ -0,0 +1,38 @@
+(*------------------------------------------------------------------------------
+  This file is part of Dom_react.
+
+  Dom_react is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Foobar is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+  ----------------------------------------------------------------------------*)
+
+
+open Dom_react
+
+let js = Js.string
+
+let onload () =
+  let (>>=) = Js.Opt.bind in
+  let return = Js.Opt.return in
+  let _ = Dom_html.document##getElementById (js"body") >>=
+    (fun body ->
+      let input1,_ = Input.Value.int 123 in
+      let input2,_ = Input.Value.int 42 in
+      Dom.appendChild body input1;
+      Dom.appendChild body input2;
+      return ()) in
+  ()
+;;
+
+let () =
+  let e = Dom_react.E.create Dom_html.window Dom_react.E.onload in
+  ignore(E.map onload e)
