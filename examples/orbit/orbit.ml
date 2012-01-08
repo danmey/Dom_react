285fb67fc563bae3e4c9e64b87cfc5c1f004df59
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-01-08 00:19:30 +0000

    Rework orbit example so it's starting to work

--- examples/orbit/orbit.ml
+++ examples/orbit/orbit.ml
@@ -1,6 +1,7 @@
 (* Converted from Jake Donham OCamljs Froc example *)
 
-open Lwt_react
+module Input = Dom_react.Input
+module S = React.S
 
 let build_list n f =
   if n < 0 then invalid_arg "n";
@@ -11,36 +12,48 @@ let build_list n f =
 
 let onload _ =
   let (>>=) = Js.Opt.bind in
-  (Dom_html.document ## getElementById (Js.string "canvas")) >>=
-    (fun canvas ->
-      let canvas = ((Js.Unsafe.coerce canvas) : Dom_html.canvasElement Js.t) in
-      let ctx = canvas  ##getContext (Dom_html._2d_) in
-      Dom_react.int_value "balls" >>= (fun balls ->
-      Dom_react.int_value "red" >>= (fun red ->
-      Dom_react.int_value "blue" >>= (fun blue ->
-      Dom_react.float_value "radius" >>= (fun radius ->
-      Dom_react.float_value "speed" >>= (fun speed ->
-        let mouse = Dom_react.mouse () in
-        let ticks,_ = Dom_react.ticks 0.05 in
-        let phase =
-          S.l2 (fun ticks speed -> ticks *. speed *. 0.01) ticks speed in
-        let shapes =
-          S.l6 (fun balls radius red blue (left, top) phase ->
-            ctx ## clearRect (0., 0., 
-                              float_of_int (canvas ## clientWidth), 
-                              float_of_int (canvas ## clientHeight));
-            build_list balls 
-              (fun i ->
-                let t = 2. *. 3.1415 *. float_of_int i /. float_of_int balls +. phase in
-                let left, top = float_of_int left, float_of_int top in
-                let left, top =
-                  (left +. cos t *. 10.*.radius),
-                  (top +. sin t *. 10.*.radius) in
-                let x0, y0 = Dom_html.elementClientPosition canvas in
-                let left, top = left -. 5. -. float_of_int x0, top -. 5. -. float_of_int y0 in
-                ctx ## fillRect (left, top, 10., 10.))) balls radius red blue mouse phase in
-        Js.Opt.return ()))))));
-  Js._false
+  let _ = Dom_html.document##getElementById (Js.string"body") >>=
+    (fun body ->
+      (Dom_html.document ## getElementById (Js.string "canvas")) >>=
+        (fun canvas ->
+          let canvas = ((Js.Unsafe.coerce canvas) : Dom_html.canvasElement Js.t) in
+          let ctx = canvas  ##getContext (Dom_html._2d_) in
+          let w1, balls = Input.Create.int 5 in
+          let w2, red = Input.Create.int 1 in
+          let w3, blue = Input.Create.int 2 in
+          let w4, radius = Input.Create.float 0.5 in
+          let w5, speed = Input.Create.float 0.6 in
+
+          Dom.appendChild body w1;
+          Dom.appendChild body w2;
+          Dom.appendChild body w3;
+          Dom.appendChild body w4;
+
+          (* let ticks = Dom_react.Base.OldS.time () in *)
+
+          (* let phase = *)
+          (*   S.l2 (fun ticks speed -> ticks *. speed *. 0.01) ticks speed in *)
+
+          (* let shapes = *)
+          (*   S.l5 (fun balls radius red blue phase -> *)
+          (*     let left, top = 0, 0 in *)
+          (*     ctx ## clearRect (0., 0.,  *)
+          (*                       float_of_int (canvas ## clientWidth),  *)
+          (*                       float_of_int (canvas ## clientHeight)); *)
+          (*     let _ = build_list balls *)
+          (*       (fun i -> *)
+          (*         let t = 2. *. 3.1415 *. float_of_int i /. float_of_int balls +. phase in *)
+          (*         let left, top = float_of_int left, float_of_int top in *)
+          (*         let left, top = *)
+          (*           (left +. cos t *. 10.*.radius), *)
+          (*           (top +. sin t *. 10.*.radius) in *)
+          (*         let x0, y0 = Dom_html.elementClientPosition canvas in *)
+          (*         let left, top = left -. 5. -. float_of_int x0, top -. 5. -. float_of_int y0 in *)
+          (*         ctx ## fillRect (left, top, 10., 10.)) in *)
+          (*   ()) balls radius red blue phase  *)
+          (* in *)
+          Js.Opt.return ())) in
+      Js._false
     
 let _ = Dom_html.window ## onload <- Dom_html.handler onload 
 
