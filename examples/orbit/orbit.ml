0d85e3ed4bb4fc164fcd86cd425748617900666e
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-01-08 02:53:22 +0000

    More work on Orbit example. Colors

--- examples/orbit/orbit.ml
+++ examples/orbit/orbit.ml
@@ -12,29 +12,30 @@ let build_list n f =
 
 let onload _ =
   let (>>=) = Js.Opt.bind in
-  let _ = Dom_html.document##getElementById (Js.string"body") >>=
+  Dom_html.document##getElementById (Js.string"body") >>=
     (fun body ->
       (Dom_html.document ## getElementById (Js.string "canvas")) >>=
         (fun canvas ->
           let canvas = ((Js.Unsafe.coerce canvas) : Dom_html.canvasElement Js.t) in
           let ctx = canvas ## getContext (Dom_html._2d_) in
-          let _, balls = Input.Named.int 5 "balls" in
-          let _, red = Input.Named.int 1 "red" in
-          let _, green = Input.Named.int 1 "green" in
-          let _, blue = Input.Named.int 2 "blue"in
-          let _, radius = Input.Named.float 0.5 "radius" in
-          let _, speed = Input.Named.float 0.6 "speed" in
+          let _, radius = Input.Named.float 5. "radius" in
+          let _, speed = Input.Named.float 6. "speed" in
+          let _, balls = Input.Named.int 10 "balls" in
+          let _, red = Input.Named.int 0 "red" in
+          let _, green = Input.Named.int 0 "green" in
+          let _, blue = Input.Named.int 0 "blue"in
 
           let ticks = Dom_react.Base.OldS.time () in
           let phase =
             S.l2 (fun ticks speed -> ticks *. speed *. 0.01) ticks speed in
 
           let shapes =
-            S.l5 (fun balls radius red blue phase ->
+            S.l6 (fun balls radius red green blue phase ->
               let left, top = 100, 100 in
               ctx ## clearRect (0., 0.,
                                 float_of_int (canvas ## clientWidth),
                                 float_of_int (canvas ## clientHeight));
+              ctx ## fillStyle <- Js.string (Printf.sprintf "rgb(%d,%d,%d)" red green blue);
               let _ = build_list balls
                 (fun i ->
                   let t = 2. *. 3.1415 *. float_of_int i /. float_of_int balls +. phase in
@@ -45,9 +46,9 @@ let onload _ =
                   let x0, y0 = Dom_html.elementClientPosition canvas in
                   let left, top = left -. 5. -. float_of_int x0, top -. 5. -. float_of_int y0 in
                   ctx ## fillRect (left, top, 10., 10.)) in
-            ()) balls radius red blue phase
+            Js.Opt.return ()) balls radius red green blue phase
           in
-          Js.Opt.return ())) in
+          Js.Opt.return ()));
       Js._false
     
 let _ = Dom_html.window ## onload <- Dom_html.handler onload 
