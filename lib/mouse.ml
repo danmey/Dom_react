d39596b1fd8869c9ef787fc1811ed7f184b3069a
Wojciech Meyer <wojciech.meyer@gmail.com>
2012-01-14 22:38:06 +0000

    Add mouse handling

---
+++ lib/mouse.ml
@@ -0,0 +1,12 @@
+open Dom_html
+
+let mouse () =
+  let signal, set = React.S.create (0,0) in
+  let receive ev =
+    let mx, my = eventAbsolutePosition ev in
+    set (mx, my);
+    Js._false
+  in
+  ignore(addEventListener (document :> eventTarget Js.t) Event.mousemove (handler receive) Js._false);
+  signal
+
